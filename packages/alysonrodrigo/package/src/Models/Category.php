30b23340aa4e1988b43e53c08c539f5da3559133
Alyson Rodrigo <g.dasilva@hotmail.com>
2018-01-09 23:14:44 -0300

    Commit inicial

---
+++ packages/alysonrodrigo/package/src/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alyson
+ * Date: 09/01/2018
+ * Time: 20:48
+ */
+
+namespace AlysonRodrigo\Package\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+
+    protected $table = 'category';
+
+    protected $fillable = [
+        'name'
+    ];
+
+}
